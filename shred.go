62ca6c0a333fbfd7f0f02f2a4609939a6e1eda37
lu4p <lu4p@protonmail.com>
2019-03-05 00:27:13 +0100

    rearranged shred.go

--- shred.go
+++ shred.go
@@ -8,51 +8,26 @@ import (
 
 // Conf is a object containing all choices of the user
 type Conf struct {
-	Filepath string
-	Times    int
-	Zeros    bool
-	Remove   bool
+	Times  int
+	Zeros  bool
+	Remove bool
 }
 
 // Path shreds all files in the location of Conf.Filepath
 // recursively. If remove is set to true files will be deleted
 // after shredding. When a file is shredded its content
 // is NOT recoverable so !!USE WITH CAUTION!!
-func (conf Conf) Path() error {
-	stats, err := os.Stat(conf.Filepath)
+func (conf Conf) Path(path string) error {
+	stats, err := os.Stat(path)
 	if err != nil {
 		return err
 	} else if stats.IsDir() {
-		err := conf.Dir()
+		err := conf.Dir(path)
 		if err != nil {
 			return err
 		}
 	} else {
-		err := conf.File()
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// File overwrites a given File in the location of Conf.Filepath
-func (conf Conf) File() error {
-	fileinfo, err := os.Stat(conf.Filepath)
-	if err != nil {
-		return err
-	}
-	size := fileinfo.Size()
-	err = conf.WriteRandom(size)
-	if err != nil {
-		return err
-	}
-	err = conf.WriteZeros(size)
-	if err != nil {
-		return err
-	}
-	if conf.Remove {
-		err := os.Remove(conf.Filepath)
+		err := conf.File(path)
 		if err != nil {
 			return err
 		}
@@ -61,26 +36,50 @@ func (conf Conf) File() error {
 }
 
 // Dir overwrites every File in the location of Conf.Filepath and everything in its subdirectories
-func (conf Conf) Dir() error {
-	err := filepath.Walk(conf.Filepath, func(path string, info os.FileInfo, err error) error {
+func (conf Conf) Dir(path string) error {
+	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		stats, _ := os.Stat(path)
 
 		if stats.IsDir() == false {
-			conf.File()
+			conf.File(path)
 		}
 		return nil
 	})
 	return err
 }
 
+// File overwrites a given File in the location of Conf.Filepath
+func (conf Conf) File(path string) error {
+	fileinfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	size := fileinfo.Size()
+	err = conf.WriteRandom(path, size)
+	if err != nil {
+		return err
+	}
+	err = conf.WriteZeros(path, size)
+	if err != nil {
+		return err
+	}
+	if conf.Remove {
+		err := os.Remove(path)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 // WriteRandom overwrites a File with random stuff.
 // conf.Times specifies how many times the file should be overwritten
-func (conf Conf) WriteRandom(size int64) error {
+func (conf Conf) WriteRandom(path string, size int64) error {
 	for i := 0; i < conf.Times; i++ {
-		file, err := os.OpenFile(conf.Filepath, os.O_RDWR, 0)
+		file, err := os.OpenFile(path, os.O_RDWR, 0)
 		defer file.Close()
 		if err != nil {
 			return err
@@ -98,11 +97,11 @@ func (conf Conf) WriteRandom(size int64) error {
 }
 
 // WriteZeros overwrites a File with zeros if conf.Zeros == true
-func (conf Conf) WriteZeros(size int64) error {
+func (conf Conf) WriteZeros(path string, size int64) error {
 	if conf.Zeros == false {
 		return nil
 	}
-	file, err := os.OpenFile(conf.Filepath, os.O_RDWR, 0)
+	file, err := os.OpenFile(path, os.O_RDWR, 0)
 	defer file.Close()
 	if err != nil {
 		return err
